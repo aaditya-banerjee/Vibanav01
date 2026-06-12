c12d251318fdf4fe5c392d539471e05c8c877801
aaditya-banerjee <aaditya.banerjee@gmail.com>
2026-06-12 21:40:51 +0000

    UI: Rebrand to Vibana and implement Myntra-style sticky navigation

--- core/urls.py
+++ core/urls.py
@@ -8,3 +8,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('', include('store.urls')),
 ]
+# Rebrand the Django Admin Dashboard
+from django.contrib import admin
+admin.site.site_header = "Vibana Administration"
+admin.site.site_title = "Vibana Admin Portal"
+admin.site.index_title = "Welcome to the Vibana Store Manager"
